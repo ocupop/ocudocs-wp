7b5b98471003628a4cb5b2f411c60d1728dd78a1
Matthew McVickar <matthew@matthewmcvickar.com>
2014-02-18 11:08:07 -0800

    Edit button.

--- wordpress/wp-content/themes/ocudocs/page.php
+++ wordpress/wp-content/themes/ocudocs/page.php
@@ -4,6 +4,7 @@
   <?php
   if ( have_posts() ) while ( have_posts() ) {
     the_post();
+    edit_post_link('edit page', '<span class="edit-link">', '</span>');
     echo "<h1>" . get_the_title() . "</h1>";
     the_content();
   }
